66f55b86cbfe2ada2863c4fab13782b9e1cb1462
Julian K. Arni <julian.arni@celeraone.com>
2014-09-15 17:11:30 +0200

    Add Haskell and Python stubs

---
+++ src/Text/Garnett/HaskellWriter.hs
@@ -0,0 +1,13 @@
+{- |
+    Module : Text.Garnett.HaskellWriter
+    Copyright : Copyright (C) 2014 Julian K. Arni
+    License : BSD3
+    Maintainer : Julian K. Arni <jkarni@gmail.com
+    Stability : alpha
+
+Convert a GarnettFile to a Haskell option-parsing module
+-}
+
+module Text.Garnett.HaskellWriter where
+
+

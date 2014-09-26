556bb39288eff1e0a02df1105994c4a4960a93da
Matthias Fischmann <mf@zerobuzz.net>
2014-09-26 17:56:01 +0200

    Typo

--- src/Text/Garnett/Writers/HaskellWriter.hs
+++ src/Text/Garnett/Writers/HaskellWriter.hs
@@ -2,12 +2,10 @@
     Module : Text.Garnett.Writers.HaskellWriter
     Copyright : Copyright (C) 2014 Julian K. Arni
     License : BSD3
-    Maintainer : Julian K. Arni <jkarni@gmail.com
+    Maintainer : Julian K. Arni <jkarni@gmail.com>
     Stability : alpha
 
 Convert a GarnettFile to a Haskell option-parsing module
 -}
 
 module Text.Garnett.Writers.HaskellWriter where
-
-

1080bc26faf069d001c35b06e00e4df348369270
Matthias Fischmann <mf@zerobuzz.net>
2014-09-26 21:41:55 +0200

    ...

--- src/Text/Garnett/Writers/HaskellWriter.hs
+++ src/Text/Garnett/Writers/HaskellWriter.hs
@@ -11,15 +11,20 @@ Convert a GarnettFile to a Haskell option-parsing module
 
 module Text.Garnett.Writers.HaskellWriter where
 
+import Data.List
+import Control.Monad
 import Data.Char
+import Control.Applicative
 import Data.String.Conversions
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 import Text.Garnett.Definition
 
 
-writer :: GarnettFile -> String  -- should this be @Text.PrettyPrint.Free.Doc e@?
-writer gf = pprint $ [buildType (_mainParser gf), buildFun (_mainParser gf)]
+writer :: GarnettFile -> IO String  -- should this be @Text.PrettyPrint.Free.Doc e@?
+writer gf = runQ $ (intercalate "\n" <$>) . sequence $ pprint <$$> [buildType (_mainParser gf), buildFun (_mainParser gf)]
+
+(<$$>) = fmap . fmap
 
 
 {-
@@ -32,8 +37,8 @@ garnettParse = GarnettOptions
 -}
 
 
-buildType :: GParser -> Dec
-buildType gparser = DataD [] dataName [] [RecC dataName fields] []
+buildType :: GParser -> Q Dec
+buildType gparser = return $ DataD [] dataName [] [RecC dataName fields] []
   where
     dataName = mkName . camelizeCap . cs $ _parserName gparser
     fields = map f $ _options gparser
@@ -52,10 +57,47 @@ buildType gparser = DataD [] dataName [] [RecC dataName fields] []
         g (Just (OITList t)) = AppT ListT (g (Just t))
 
 
-buildFun :: GParser -> Dec
-buildFun _ = FunD (mkName "garnettParse") [] -- [mkName "GarnettOptions"]
+buildFun :: GParser -> Q Dec
+buildFun gparser = do
+    let dataName = mkName . camelizeCap . cs $ _parserName gparser
+        funName = mkName . ("parseOpt" <>) . camelizeCap . cs $ _parserName gparser
+
+        fields = map f $ _options gparser
+          where
+            f :: Option -> Exp
+            f option = error "wef"  --- (mkName . cs $ _optName option, NotStrict, g $ _input option)
+
+    body <- [| VarE (mkName "should_be_dataName") |]
+
+    return $ FunD funName [Clause [] (NormalB body) []]
+
+
+
+
+{-
+
+    body :: Q Exp
+    body = error "wef"  --   -- ) (VarE $ mkName "<$>")
+
+
+data Main
+    = Main {verbosity :: GHC.Types.Bool,
+            help :: GHC.Types.Bool,
+            inputFile :: ([GHC.IO.FilePath]),
+            logLevel :: GHC.Types.Bool}
+
+parseOptMain = Main <$>
+
+
+-}
+
+
 
 
 camelizeCap :: String -> String
 camelizeCap "" = ""
 camelizeCap (c:cs) = toUpper c : cs
+
+camelizeSmall :: String -> String
+camelizeSmall "" = ""
+camelizeSmall (c:cs) = toLower c : cs
